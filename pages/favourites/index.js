5bb6b71fb0e4ceca2dd0be278ff93447498237ba
delissgt <delissgt@gmail.com>
2021-06-09 18:55:34 -0500

    Added Logic in Favourite Page

--- pages/favourites/index.js
+++ pages/favourites/index.js
@@ -1,6 +1,44 @@
+import React, {useContext} from "react";
+import { Store } from "../../context/StoreProvider";
+
+import EpisodeList from "../../components/EpisodesList/EpisodesList";
+
+import styles from "../../styles/Home.module.css";
 
 export default function FavouritesPage(){
+  const { state, dispatch } = useContext(Store);
+
+  // creating action
+  const toggleFavAction = (episode) => {
+    const episodeInFavourites = state.favourites.includes(episode);
+    let dispatchObj = {
+      type: "ADD_FAV",
+      payload: episode
+    };
+
+    if (episodeInFavourites) {
+      const favoritesWithOutEpisode = state.favourites.filter( fav => fav.id !== episode.id )
+      console.log("faWithOut", favoritesWithOutEpisode);
+      dispatchObj = {
+        type: "REMOVE_FAV",
+        payload: favoritesWithOutEpisode
+      };
+    }
+    return dispatch(dispatchObj);
+  }
+
+  const props = {
+    episodes: state.favourites,
+    toggleFavAction: toggleFavAction,
+    favourites: state.favourites
+  };
+
+
   return(
-      <div>Favourites pages</div>
+      <React.Fragment>
+        <div className={styles.episodeLayout}>
+          <EpisodeList {...props}/>
+        </div>
+      </React.Fragment>
   )
 }
